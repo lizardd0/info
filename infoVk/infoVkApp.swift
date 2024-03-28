3209dd2f8582bb3a950d3688903fae0a1d9e8028
Елизавета Шерман <lizasherpro001@gmail.com>
2024-03-28 16:55:12 +0300

    Initial commit

---
+++ infoVk/infoVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  infoVkApp.swift
+//  infoVk
+//
+//  Created by Елизавета Шерман on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct infoVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
